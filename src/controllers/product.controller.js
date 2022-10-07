0103e225ba5a7a659a92d525d7c012de5a0609db
Marco Valdez <antem.marco@gmail.com>
2022-10-06 21:34:49 -0500

    feat: Adicion de Update y Delete. Realizacion de Pruebas

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -35,7 +35,9 @@ const createBook = async (req, res) => {
     // TryCatch para conexion y acceso a la base de datos
     try {
         // Se llama a la funcion y se guarda la respuesta en query
-        const query = await bookServices.createBook(req.params, res);
+        // Se guardan los valores del Body de la Request en Variables para ser enviadas al servicio
+        const { id, title, total_pages, author, nationality, language, cover_url, editorial } = req.body;
+        const query = await bookServices.createBook(id, title, total_pages, author, nationality, language, cover_url, editorial, res);
         return query;
     } catch (error) {
         // En caso de error, se regresa un status y se muestra
@@ -45,10 +47,30 @@ const createBook = async (req, res) => {
 };
 
 // Metodo UpdateBook que actualiza un libro en conciso (Req: Id + Parametros)
-const updateBook = async (req, res) => {};
+const updateBook = async (req, res) => {
+    try {
+        const { title, total_pages, author, nationality, language, cover_url, editorial } = req.body;
+        const query = await bookServices.updateBook(req.params, title, total_pages, author, nationality, language, cover_url, editorial, res);
+        return query;
+    } catch (error) {
+        res.status(400);
+        res.send(error.message);
+    }
+};
 
 // Metodo DeleteBook que elimina un libro en conciso (Req: Id)
-const deleteBook = async (req, res) => {};
+const deleteBook = async (req, res) => {
+    // TryCatch para conexion y acceso a la base de datos
+    try {
+        // Se llama a la funcion y se guarda la respuesta en query
+        const query = await bookServices.deleteBook(req.params, res);
+        return query;
+    } catch (error) {
+        // En caso de error, se regresa un status y se muestra
+        res.status(400);
+        res.send(error.message);
+    }
+};
 
 // Exportar Metodos
 export const methods = {
--- src/services/product.services.js
+++ src/services/product.services.js
@@ -26,32 +26,63 @@ async function readBook(body, response) {
 }
 
 // Funcion createBook la cual contiene el QuerySQL para crear un Libro en la Base de Datos
-// Requiere: Endpoint, JSON-Body
-async function createBook(body, response) {
+// Requiere: Endpoint, JSON-Body (Variables con los Datos)
+async function createBook(id, title, total_pages, author, nationality, language, cover_url, editorial, response) {
+    // If de verificacion de casillas completadas
     if (
-        body.id === undefined ||
-        body.title === undefined ||
-        body.total_pages === undefined ||
-        body.author === undefined ||
-        body.nationality === undefined ||
-        body.language === undefined ||
-        body.cover_url === undefined ||
-        body.editorial === undefined
+        id === undefined ||
+        title === undefined ||
+        total_pages === undefined ||
+        author === undefined ||
+        nationality === undefined ||
+        language === undefined ||
+        cover_url === undefined ||
+        editorial === undefined
     ) {
+        return response.status(400).json({ message: "Bad Request. Check the fields and try again" });
     }
     const connection = await getConnection();
-    // Query SQL donde especifica: SELECCIONAR TODOS DESDE books
-    const result = await connection.query("SELECT * FROM books");
+    // Query SQL donde especifica: INSERTAR en la tabla books (Parametros) los valores (Indice de Parametros) + (Valores de Parametros)
+    const result = await connection.query(
+        "INSERT INTO `books` (`id`, `title`, `total_pages`, `author`, `nationality`, `language`, `cover_url`, `editorial`) VALUES (?,?,?,?,?,?,?,?)",
+        [id, title, total_pages, author, nationality, language, cover_url, editorial]
+    );
     return response.json(result);
 }
 
 // Funcion updateBook la cual contiene el QuerySQL para cambiar valores de un libro existente
 // Requiere: Endpoint, ID, JSON-Body
-async function updateBook(body, response) {}
+async function updateBook(body, title, total_pages, author, nationality, language, cover_url, editorial, response) {
+    // If de verificacion de casillas completadas
+    if (
+        title === undefined ||
+        total_pages === undefined ||
+        author === undefined ||
+        nationality === undefined ||
+        language === undefined ||
+        cover_url === undefined ||
+        editorial === undefined
+    ) {
+        return response.status(400).json({ message: "Bad Request. Check the fields and try again" });
+    }
+    const connection = await getConnection();
+    // Query SQL donde especifica: ACTUALIZAR en la tabla books y COLOCAR en X parametro Y valor
+    const result = await connection.query(
+        "UPDATE `books` SET `title`=?, `total_pages`=?, `author`=?, `nationality`=?, `language`=?, `cover_url`=?, `editorial`=? WHERE id=?",
+        [title, total_pages, author, nationality, language, cover_url, editorial, body.id]
+    );
+    return response.json(result);
+}
 
 // Funcion deleteBook la cual contiene el QuerySQL para eliminar un libro especifico
 // Requiere: Endpoint, ID
-async function deleteBook(body, response) {}
+async function deleteBook(body, response) {
+    const connection = await getConnection();
+    console.log(body.id);
+    // Query SQL donde especifica: SELECCIONAR TODOS DESDE books DONDE EL ID SEA <id>
+    const result = await connection.query("DELETE FROM books WHERE id = ?", body.id);
+    return response.json(result);
+}
 
 export const methods = {
     readBooks,
